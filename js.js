9a91a78064de06676d00172c4e0b236c0183b5d9
Rahul Niraula <rahul.niraula7@live.com>
2017-12-16 22:17:59 +0545

    as

--- js.js
+++ js.js
@@ -1,4 +1,25 @@
-
 $(document).ready(function(){
-  alert("Hi");
+  makeFixed("#AdSense1");
 });
+
+function makeFixed(item){
+  var i=$(item);
+  $(window).scroll(function(){
+    var scrollTop= $(window).scrollTop();
+    var dis=$(i).offset().top;
+    var diff=scrollTop-dis;
+    console.log(diff);
+    if(diff>5){
+      console.log(scrollTop);
+      $(i).css({
+        'position':'fixed',
+
+      });
+    }else{
+
+      $(i).css({'position':'relative','z-index':1});
+
+    }
+
+  });
+}
